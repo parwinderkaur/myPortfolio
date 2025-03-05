f20a143bf5ea5947586279803d23eda5e1b0371b
Parwinderkaur385 <parwinderkaur385@gmail.com>
2025-03-05 13:07:57 +0530

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,146 @@
+
+const navLinks = document.querySelectorAll("header nav a");
+const logoLink = document.querySelector(".logo");
+const sections = document.querySelectorAll("section");
+const menuIcon = document.querySelector("#menu-icon");
+const navBar = document.querySelector("header nav");
+
+const colorBtn1 = document.querySelector('.box1');
+const colorBtn2 = document.querySelector('.box2');
+const colorBtn3 = document.querySelector('.box3');
+const colorBtn4 = document.querySelector('.box4');
+
+colorBtn1.addEventListener('click', () => {
+    document.documentElement.style.setProperty('--main-color', '#ff7b00')
+})
+
+colorBtn2.addEventListener('click', () => {
+    document.documentElement.style.setProperty('--main-color', '#00c3ff')
+})
+
+colorBtn3.addEventListener('click', () => {
+    document.documentElement.style.setProperty('--main-color', '#f5b400')
+})
+
+colorBtn4.addEventListener('click', () => {
+    document.documentElement.style.setProperty('--main-color', '#06bb9d')
+})
+
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("ri-close-large-line");
+  navBar.classList.toggle("active");
+});
+
+const activePage = () => {
+  const header = document.querySelector("header");
+  const barsBox = document.querySelector(".bars-box");
+
+  header.classList.remove("active");
+  setTimeout(() => {
+    header.classList.add("active");
+  }, 1100);
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+  });
+
+  barsBox.classList.remove("active");
+  setTimeout(() => {
+    barsBox.classList.add("active");
+  }, 1100);
+
+  sections.forEach((section) => {
+    section.classList.remove("active");
+  });
+
+  menuIcon.classList.remove("ri-close-large-line");
+  navBar.classList.remove("active");
+};
+
+navLinks.forEach((link, idx) => {
+  link.addEventListener("click", () => {
+    if (!link.classList.contains("active")) {
+      activePage();
+      link.classList.add("active");
+
+      setTimeout(() => {
+        sections[idx].classList.add("active");
+      }, 1100);
+    }
+  });
+});
+
+logoLink.addEventListener("click", () => {
+  if (!navLinks[0].classList.contains("active")) {
+    activePage();
+    navLinks[0].classList.add("active");
+
+    setTimeout(() => {
+      sections[0].classList.add("active");
+    }, 1100);
+  }
+});
+
+const resumeBtns = document.querySelectorAll(".resume-btn");
+
+resumeBtns.forEach((btn, idx) => {
+  btn.addEventListener("click", () => {
+    const resumeDetails = document.querySelectorAll(".resume-detail");
+
+    resumeBtns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    btn.classList.add("active");
+
+    resumeDetails.forEach((detail) => {
+      detail.classList.remove("active");
+    });
+    resumeDetails[idx].classList.add("active");
+  });
+});
+
+const arrowRight = document.querySelector(
+  ".portfolio-box .navigation .arrow-right"
+);
+const arrowLeft = document.querySelector(
+  ".portfolio-box .navigation .arrow-left"
+);
+
+let index = 0;
+
+const activePortfolio = () => {
+  const imgSlide = document.querySelector(".portfolio-carousel .img-slide");
+  const portfolioDetails = document.querySelectorAll(".portfolio-detail");
+
+  imgSlide.style.transform = `translateX(calc(${index * -100}% - ${
+    index * 2
+  }rem))`;
+
+  portfolioDetails.forEach((detail) => {
+    detail.classList.remove("active");
+  });
+  portfolioDetails[index].classList.add("active");
+};
+
+arrowRight.addEventListener("click", () => {
+  if (index < 6) {
+    index++;
+    arrowLeft.classList.remove("disabled");
+  } else {
+    index = 7;
+    arrowRight.classList.add("disabled");
+  }
+  activePortfolio();
+});
+
+arrowLeft.addEventListener("click", () => {
+  if (index > 1) {
+    index--;
+    arrowRight.classList.remove("disabled");
+  } else {
+    index = 0;
+    arrowLeft.classList.add("disabled");
+  }
+  activePortfolio();
+});
